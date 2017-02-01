9dc9287b294e6095e411031082daa1831a06f292
Javier <fj.gutierrezs91@gmail.com>
2017-02-01 11:02:51 +0100

    Grid...

--- app/src/main/java/es/domainjavito/gitexample/GitActivity.java
+++ app/src/main/java/es/domainjavito/gitexample/GitActivity.java
@@ -1,13 +1,116 @@
 package es.domainjavito.gitexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class GitActivity extends AppCompatActivity {
+public class GitActivity extends Activity {
+
+    private static String TAG = "Debug Callback() : ";
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        // references to our images...
+        private Integer[] mThumbIds;
+
+        private static final int MAX_FIELDS = 12;
+        private static final int MAX_ROWS = 20;
+        private static final int MAX_BOXES = MAX_FIELDS * MAX_ROWS;
+
+        public ImageAdapter(Context c) {
+            mThumbIds = new Integer[MAX_BOXES];
+
+            for (int i = 0 ; i < MAX_BOXES ; i++) {
+                mThumbIds[i] = R.drawable.defimage;
+            }
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return mThumbIds[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes.
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_git);
+        setContentView(R.layout.grid_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GitActivity.this, "Box : " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy()");
     }
 }
